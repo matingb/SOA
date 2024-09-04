9dac06a0f887fdf62fff360221629e0168c6c8b0
Matias Garcia <43641235+matingb@users.noreply.github.com>
2024-09-03 22:20:39 -0300

    Update arduino.ino
    agrega switch con estados comentado

--- arduino.ino
+++ arduino.ino
@@ -8,7 +8,7 @@
 
 const int ENCENDER = HIGH;
 const int APAGAR = LOW;
-const int LED = 2; 
+const int LED = 2;
 const int PULSADOR = 12;
 const int POTENCIOMETRO = A4;
 const int SENSOR_TEMPERATURA = A5;
@@ -16,16 +16,94 @@ float temperaturaEsperada;
 float temperatura;
 int boton;
 
-
-void setup() { 
+void setup() {
   Serial.begin(9600);
   pinMode(PULSADOR,INPUT);
   pinMode(SENSOR_TEMPERATURA, INPUT);
   pinMode(LED, OUTPUT);
-} 
+}
 
 void loop() {
+	fsm();
+}
+
+void fsm() {
   leerEventos();
+
+  /*switch(estadoActual) {
+    case ESTADO_NO_PREPARADO:
+        switch(evento) {
+            case EVENTO_ENCENDER:
+                enviarNotificacionError();
+                estadoActual = ESTADO_NO_PREPARADO;
+                break;
+            case EVENTO_AGUA_SUFICIENTE:
+                estadoActual = ESTADO_PREPARADO;
+                break;
+            case EVENTO_CONTINUE:
+                estadoActual = ESTADO_NO_PREPARADO;
+                break;
+            default:
+                break;
+        }
+        break;
+    case ESTADO_PREPARADO:
+        switch(evento)
+            case EVENTO_ENCENDER:
+                encenderCalentador();
+                regularIntensidadRGBRojo();
+                estadoActual = ESTADO_CALENTANDO;
+                break;
+            case EVENTO_AGUA_NO_SUFICIENTE:
+                estadoActual = ESTADO_NO_PREPARADO;
+                break;
+            case EVENTO_CONTINUE:
+                estadoActual = ESTADO_PREPARADO;
+                break;
+            default:
+                break;
+    case ESTADO_CALENTANDO:
+        switch(evento)
+            case EVENTO_TEMPERATURA_DESEADA_ALCANZADA:
+                apagarCalentador();
+                enviarNotificacion();
+                prenderRBGVerde();
+                estadoActual = ESTADO_FINALIZADO;
+                break;
+            case EVENTO_APAGAR:
+                apagarCalentador();
+                apagarRGB();
+                estadoActual = ESTADO_NO_PREPARADO;
+            case EVENTO_CONTINUE:
+                regularIntensidadRGBRojo();
+                estadoActual = ESTADO_CALENTANDO;
+                break;
+            default:
+                break;
+    case ESTADO_FINALIZADO:
+        switch(evento)
+            case EVENTO_PERDIDA_TEMPERATURA_DESEADA:
+                encenderCalentador();
+                regularIntensidadRGBRojo();
+                estadoActual = ESTADO_CALENTANDO;
+                break;
+            case EVENTO_APAGAR:
+                apagarCalentador();
+                apagarRGB();
+                estadoActual = ESTADO_NO_PREPARADO;
+                break;
+            case EVENTO_CONTINUE:
+                estadoActual = ESTADO_FINALIZADO;
+                break;
+            default:
+                break;
+  }*/
+}
+
+void leerEventos() {
+  leerEncendido();
+  leerTemperatura();
+
   if(boton == HIGH) {
     encenderCalentador();
   }
@@ -33,11 +111,6 @@ void loop() {
   if(temperatura > temperaturaEsperada) {
     apagarCalentador();
   }
-}
-
-void leerEventos() {
-  leerEncendido();
-  leerTemperatura();
 }
 
 void leerEncendido() {
