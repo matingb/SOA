91127b26a1e8b5d95f13cf5104f2f80af9a350d1
Federico Castro <federico2002castro@gmail.com>
2024-11-09 16:55:11 -0300

    Bluetooth desconectado

--- app/src/main/java/com/example/smartboiler/BluetoothReceiver.java
+++ app/src/main/java/com/example/smartboiler/BluetoothReceiver.java
@@ -15,26 +15,11 @@ public class BluetoothReceiver extends BroadcastReceiver {
         if (BluetoothAdapter.ACTION_STATE_CHANGED.equals(action)) {
             int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR);
 
-            switch (state) {
-                case BluetoothAdapter.STATE_OFF:
-                    Toast.makeText(context, "Bluetooth apagado", Toast.LENGTH_SHORT).show();
-                    break;
-
-                case BluetoothAdapter.STATE_TURNING_OFF:
-                    Toast.makeText(context, "Bluetooth apagándose", Toast.LENGTH_SHORT).show();
-                    break;
-
-                case BluetoothAdapter.STATE_ON:
-                    Toast.makeText(context, "Bluetooth encendido", Toast.LENGTH_SHORT).show();
-                    break;
-
-                case BluetoothAdapter.STATE_TURNING_ON:
-                    Toast.makeText(context, "Bluetooth encendiéndose", Toast.LENGTH_SHORT).show();
-                    break;
-
-                default:
-                    Toast.makeText(context, "Estado desconocido", Toast.LENGTH_SHORT).show();
-                    break;
+            if(state == BluetoothAdapter.STATE_OFF) {
+                Toast.makeText(context, "Bluetooth apagado", Toast.LENGTH_SHORT).show();
+                intent = new Intent(context, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(intent);
             }
         }
     }
